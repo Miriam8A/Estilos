510ca94d1dabaad257d59a705f42323cfc7c8e61
Armando Velarde <armaninteresado@hotmail.com>
2021-10-13 21:27:02 -0500

    agregando contacto

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Estilos.Models;
 
 namespace Estilos.Data
 {
@@ -12,5 +13,7 @@ namespace Estilos.Data
             : base(options)
         {
         }
+
+        public DbSet<Estilos.Models.Contact> DataContactos { get; set;}
     }
 }
