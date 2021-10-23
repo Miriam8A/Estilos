8e2b278556efcbdd929551016adcf745007d3df8
Armando Velarde <armaninteresado@hotmail.com>
2021-10-23 04:14:40 -0500

    agregando producto

--- Controllers/CatalogoController.cs
+++ Controllers/CatalogoController.cs
@@ -13,11 +13,13 @@ namespace Estilos.Controllers
     {
         private readonly ApplicationDbContext _context;
         private readonly UserManager<IdentityUser> _userManager;
+        private readonly UserManager<IdentityUser> _userManager2;
 
-        public CatalogoController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        public CatalogoController(ApplicationDbContext context, UserManager<IdentityUser> userManager, UserManager<IdentityUser> userManager2)
         {
             _context = context;
             _userManager = userManager;
+            _userManager2 = userManager2;
         }
 
         public async Task<IActionResult> Index()
@@ -66,6 +68,25 @@ namespace Estilos.Controllers
                 _context.Add(proforma);
                 await _context.SaveChangesAsync();
                 return  RedirectToAction(nameof(Index));
+            }
+        }
+        public async Task<IActionResult> Add2(int? id)
+        {
+            var userID = _userManager2.GetUserName(User);
+            if(userID == null){
+                ViewData["Message"] = "Por favor debe loguearse antes de agregar un producto";
+                List<Product2> productos = new List<Product2>();
+                return  View("Index2",productos);
+            }else{
+                var producto2 = await _context.DataProducts2.FindAsync(id);
+                Proforma2 proforma2 = new Proforma2();
+                proforma2.Producto = producto2;
+                proforma2.Price = producto2.Price;
+                proforma2.Quantity = 1;
+                proforma2.UserID = userID;
+                _context.Add(proforma2);
+                await _context.SaveChangesAsync();
+                return  RedirectToAction(nameof(Index2));
             }
         }
     }
