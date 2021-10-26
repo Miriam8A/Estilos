c90ff1c56e105a2b32b70cd842b905f1f3aeac90
Armando Velarde <armaninteresado@hotmail.com>
2021-10-25 19:05:39 -0500

    Modificacion de Catalogo

--- Controllers/CatalogoController.cs
+++ Controllers/CatalogoController.cs
@@ -13,25 +13,23 @@ namespace Estilos.Controllers
     {
         private readonly ApplicationDbContext _context;
         private readonly UserManager<IdentityUser> _userManager;
-        private readonly UserManager<IdentityUser> _userManager2;
 
-        public CatalogoController(ApplicationDbContext context, UserManager<IdentityUser> userManager, UserManager<IdentityUser> userManager2)
+        public CatalogoController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
         {
             _context = context;
             _userManager = userManager;
-            _userManager2 = userManager2;
+            
+        }
+        public IActionResult CatalogoD()
+        {
+            var productos = _context.DataProducts.Where(x=> x.Categoria == "D");
+            return View(productos);
         }
 
-        public async Task<IActionResult> Index()
+        public IActionResult CatalogoC()
         {
-            var productos = from o in _context.DataProducts select o;
-            return View(await productos.ToListAsync());
-        }
-
-        public async Task<IActionResult> Index2()
-        {
-            var productos = from o in _context.DataProducts2 select o;
-            return View(await productos.ToListAsync());
+            var productos = _context.DataProducts.Where(x=> x.Categoria == "C");
+            return View(productos);
         }
 
         public async Task<IActionResult> Details(int id)
@@ -45,7 +43,7 @@ namespace Estilos.Controllers
 
         public async Task<IActionResult> Details2(int id)
         {
-           Product2 objProduct2 = await _context.DataProducts2.FindAsync(id);
+           Product objProduct2 = await _context.DataProducts.FindAsync(id);
            if(objProduct2 == null){
                return NotFound();
            }
@@ -57,7 +55,7 @@ namespace Estilos.Controllers
             if(userID == null){
                 ViewData["Message"] = "Por favor debe loguearse antes de agregar un producto";
                 List<Product> productos = new List<Product>();
-                return  View("Index",productos);
+                return  View("CatalogoD",productos);
             }else{
                 var producto = await _context.DataProducts.FindAsync(id);
                 Proforma proforma = new Proforma();
@@ -67,26 +65,26 @@ namespace Estilos.Controllers
                 proforma.UserID = userID;
                 _context.Add(proforma);
                 await _context.SaveChangesAsync();
-                return  RedirectToAction(nameof(Index));
+                return  RedirectToAction(nameof(CatalogoD));
             }
         }
         public async Task<IActionResult> Add2(int? id)
         {
-            var userID = _userManager2.GetUserName(User);
+            var userID = _userManager.GetUserName(User);
             if(userID == null){
                 ViewData["Message"] = "Por favor debe loguearse antes de agregar un producto";
-                List<Product2> productos = new List<Product2>();
-                return  View("Index2",productos);
+                List<Product> productos = new List<Product>();
+                return  View("CatalogoC",productos);
             }else{
-                var producto2 = await _context.DataProducts2.FindAsync(id);
-                Proforma2 proforma2 = new Proforma2();
-                proforma2.Producto = producto2;
-                proforma2.Price = producto2.Price;
-                proforma2.Quantity = 1;
-                proforma2.UserID = userID;
-                _context.Add(proforma2);
+                var producto = await _context.DataProducts.FindAsync(id);
+                Proforma proforma = new Proforma();
+                proforma.Producto = producto;
+                proforma.Price = producto.Price;
+                proforma.Quantity = 1;
+                proforma.UserID = userID;
+                _context.Add(proforma);
                 await _context.SaveChangesAsync();
-                return  RedirectToAction(nameof(Index2));
+                return  RedirectToAction(nameof(CatalogoC));
             }
         }
     }
